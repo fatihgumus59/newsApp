9aa6c221ca5831096f2cdf1c8be01662d41dcdac
fatihgumus59 <fatihgumus72@gmail.com>
2022-04-03 15:36:51 +0300

    project started

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View, Text, SafeAreaView } from 'react-native';
+
+const App = () => {
+
+    return (
+        <SafeAreaView>
+            <View>
+                <Text>Start!</Text>
+            </View>  
+        </SafeAreaView>
+    )
+}
+
+export default App;
+
